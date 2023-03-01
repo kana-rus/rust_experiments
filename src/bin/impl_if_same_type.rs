c1275b6f48e44268ef8ad1d5be1b0ba5b3f52458
kana-rus <kanarus786@gmail.com>
2023-03-01 16:20:03 +0900

    notworksasexpected

--- src/bin/impl_if_same_type.rs
+++ src/bin/impl_if_same_type.rs
@@ -4,30 +4,6 @@
 use std::ops::{Add, Sub, AddAssign};
 use std::any::type_name;
 
-struct S(usize);
-struct T(usize);
-struct Out(usize);
-
-type Alias = T;
-
-impl Add<T> for S {
-    type Output = Out;
-    fn add(self, rhs: T) -> Self::Output {
-        Out(self.0 + rhs.0)
-    }
-}
-
-
-
-
-
-
-
-
-
-struct A(usize);
-struct B(usize);
-
 const fn eq(lhstr: &'static str, rhstr: &'static str) -> bool {
     let lhs = lhstr.as_bytes();
     let rhs = rhstr.as_bytes();
@@ -47,60 +23,37 @@ const fn eq(lhstr: &'static str, rhstr: &'static str) -> bool {
     true
 }
 
-/*
-fn sample_1() {
-    struct X(usize);
+#[derive(Debug, PartialEq, Clone)]
+struct A(usize);
+struct B(usize);
 
-    const _: (/* for input
-    ```
-        impl Add<B> for A {
-            type Output = X;
-            fn add(self, rhs: B) -> Self::Output {
-                X(self.0 + rhs.0)
-            }
-        }
-    ```
-    */) = {
-        const fn impl_add_if_same_type() {
-            if eq(type_name::<X>(), type_name::<A>()) {
-                {
-                    impl Add<B> for A {
-                        type Output = X;
-                        fn add(self, rhs: B) -> Self::Output {
-                            X(self.0 + rhs.0)
-                        }
-                    }
-                    impl AddAssign<B> for A {
-                        fn add_assign(&mut self, rhs: B) {
-                            self = *self + rhs
-                        }
-                    }
-                }
-            } else {
-                {
-
-                }
-            }
-        }
-        impl_add_if_same_type()
-    };
-}
-*/
-
-/* 
 impl Add<B> for A {
     type Output = X;
     fn add(self, rhs: B) -> Self::Output {
-        X(self.0 + rhs.0)
+        A(self.0 + rhs.0)
     }
 }
 
-struct X(usize);
+type X = A;
 
-#[cfg(eq())]
-impl AddAssign<B> for A {
+const _: () = {
+    const fn impl_assgin_if_same_type() {
+        if eq(type_name::<A>(), type_name::<<A as Add<B>>::Output>()) {
+            impl AddAssign<B> for A {
+                fn add_assign(&mut self, rhs: B) {
+                    *self = self.clone() + rhs
+                }
+            }
+        }
+    }
+    //impl_assgin_if_same_type()
+};
 
+fn main() {
+    let x = A(100) + B(200);
+    assert_eq!(x, A(300));
+
+    let mut a = A(100);
+    a += B(200);
+    assert_eq!(a, A(300));
 }
-*/
-
-fn main() {}
