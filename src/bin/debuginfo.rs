235137a95e80707ad6e8856a9b6f3aab65686402
kana-rus <kanarus786@gmail.com>
2023-03-29 16:36:15 +0900

    Add: debuginfo.rs

---
+++ src/bin/debuginfo.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let _file:   &'static str = file!();
+    let _line:   u32          = line!();
+    let _column: u32          = column!();
+}
+
+fn __expanded__() {
+    let _file:   &'static str = "src/bin/debuginfo.rs";
+    let _line:   u32          = 3u32;
+    let _column: u32          = 33u32;
+}
