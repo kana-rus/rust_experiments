1a53a3e8900c93e1a9f0526857ba112572854ae0
kana-rus <kanarus786@gmail.com>
2023-07-30 08:08:38 +0900

    save@2023-07-30 08:08+9:00

--- benches/string_swap.rs
+++ benches/string_swap.rs
@@ -71,9 +71,9 @@ mod swappers {
     pub fn swap_chars_3(string: &mut String, m: usize, n: usize) {
         let (pos_m, char_m) = string.char_indices().nth(m).unwrap();
         let (pos_n, char_n) = string.char_indices().nth(n).unwrap();
+        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
         
         let bytes = unsafe {string.as_mut_vec()};
-        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
 
         match len_m.cmp(&len_n) {
             Ordering::Greater => {
@@ -105,9 +105,9 @@ mod swappers {
     pub fn swap_chars_3_v2(string: &mut String, m: usize, n: usize) {
         let (pos_m, char_m) = string.char_indices().nth(m).unwrap();
         let (pos_n, char_n) = string.char_indices().nth(n).unwrap();
+        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
         
         let bytes = unsafe {string.as_mut_vec()};
-        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
 
         for i in 0..len_m.min(len_n) {
             bytes.swap(pos_m+i, pos_n+i)
@@ -129,9 +129,9 @@ mod swappers {
     pub fn swap_chars_3_v3(string: &mut String, m: usize, n: usize) {
         let (pos_m, char_m) = string.char_indices().nth(m).unwrap();
         let (pos_n, char_n) = string.char_indices().nth(n).unwrap();
+        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
         
         let bytes = unsafe {string.as_mut_vec()};
-        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
 
         for i in 0..len_m.min(len_n) {
             bytes.swap(pos_m+i, pos_n+i)
@@ -147,9 +147,9 @@ mod swappers {
     pub fn swap_chars_4(string: &mut String, m: usize, n: usize) {
         let (pos_m, char_m) = string.char_indices().nth(m).unwrap();
         let (pos_n, char_n) = string.char_indices().nth(n).unwrap();
+        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
         
         let bytes = unsafe {string.as_mut_vec()};
-        let (len_m, len_n) = (char_m.len_utf8(), char_n.len_utf8());
         let (left, bytes_m, mid, bytes_n, right) = (
             &bytes[..pos_m],
             &bytes[pos_m..(pos_m+len_m)],
