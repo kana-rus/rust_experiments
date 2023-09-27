3a0bb8f5f6724eed9bab432ab57198d57dbf891c
kana-rus <kanarus786@gmail.com>
2023-09-27 21:55:09 +0900

    @2023-09-27 21:55+9:00

--- src/bin/regex.rs
+++ src/bin/regex.rs
@@ -20,16 +20,17 @@ fn main() {
     let r = Regex::new(
         // "^/api(?:$()|/users/([^/]+)$())"
         // "^/api(?:$|/users$)"
-        "^/api(?:$()|/users/([^/]+)$())"
+        "^/api(?:$()|/(?:users/([^/]+)$()|tasks/([^/]+)$()))"
     ).unwrap();
     for path in [
         "/api",
-        // "/api/users",
         "/api/users/42",
         "/api/users/kanarus",
+        "/api/tasks/42",
+        "/api/tasks/kanarus",
     ] {
         println!("\n[{path}]");
-        for c in r.captures(path).unwrap().iter() {
+        for c in r.captures(path).unwrap().iter().skip(1/*The entire match*/) {
             println!("{:?}", c);
         }
     }
