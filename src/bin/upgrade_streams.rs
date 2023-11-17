04498ae1d13e1c7ecc6742265495d68e7bb2f1f6
kana-rus <kanarus786@gmail.com>
2023-11-17 17:01:29 +0900

    @2023-11-17 17:01+9:00

--- src/bin/upgrade_streams.rs
+++ src/bin/upgrade_streams.rs
@@ -32,7 +32,7 @@ mod upgrade {
         UPGRADE_STREAMS.get_or_init(StreamLit::new)
     }
 
-    async fn reserve_upgrade() -> UpgradeID {
+    pub async fn reserve_upgrade() -> UpgradeID {
         struct ReserveUpgrade;
         impl Future for ReserveUpgrade {
             type Output = UpgradeID;
@@ -54,7 +54,7 @@ mod upgrade {
         ReserveUpgrade.await
     }
 
-    async fn set_stream(id: UpgradeID, stream: Arc<Mutex<Stream>>) {
+    pub async fn set_stream(id: UpgradeID, stream: Arc<Mutex<Stream>>) {
         struct EnsureReserved{id: UpgradeID}
         impl Future for EnsureReserved {
             type Output = ();
@@ -62,9 +62,8 @@ mod upgrade {
                 let Some(cell) = UpgradeStreams().get().get(self.id.as_usize())
                     else {cx.waker().wake_by_ref(); return std::task::Poll::Pending};
                 
-                if !cell.is_just_reserved() {
-                    cx.waker().wake_by_ref(); return std::task::Poll::Pending;
-                }
+                if !cell.is_just_reserved()
+                    {cx.waker().wake_by_ref(); return std::task::Poll::Pending};
 
                 std::task::Poll::Ready(())
             }
@@ -74,7 +73,7 @@ mod upgrade {
         (unsafe {UpgradeStreams().get_mut()})[id.as_usize()].stream = Some(stream);
     }
 
-    async fn assume_upgradable(id: UpgradeID) -> Stream {
+    pub async fn assume_upgradable(id: UpgradeID) -> Stream {
         struct AssumeUpgradable{id: UpgradeID}
         impl Future for AssumeUpgradable {
             type Output = Stream;
@@ -82,14 +81,16 @@ mod upgrade {
                 let Some(StreamCell { reserved, stream }) = (unsafe {UpgradeStreams().get_mut()}).get_mut(self.id.as_usize())
                     else {cx.waker().wake_by_ref(); return std::task::Poll::Pending};
 
-                if !stream.as_ref().is_some_and(|arc| Arc::strong_count(&arc) == 1) {
-                    cx.waker().wake_by_ref(); return std::task::Poll::Pending;
-                }
+                if !stream.as_ref().is_some_and(|arc| Arc::strong_count(&arc) == 1)
+                    {cx.waker().wake_by_ref(); return std::task::Poll::Pending};
 
-                let stream = unsafe {Mutex::into_inner(Arc::into_inner(stream.take().unwrap_unchecked()).unwrap_unchecked())};
-                *reserved  = false;
-
-                std::task::Poll::Ready(stream)
+                *reserved = false;
+                std::task::Poll::Ready(unsafe {
+                    Mutex::into_inner(
+                        Arc::into_inner(
+                            Option::take(stream)
+                                .unwrap_unchecked())
+                                    .unwrap_unchecked())})
             }
         }
 
@@ -171,10 +172,103 @@ mod upgrade {
                 self.0
             }
         }
+
+        impl std::fmt::Display for UpgradeID {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                std::fmt::Display::fmt(&self.as_usize(), f)
+            }
+        }
     };
 }
 
 
 #[tokio::main] async fn main() {
+    use upgrade::*;
+    use std::sync::Arc;
+    use tokio::sync::Mutex;
 
+    struct Context {
+        upgrade_id: Option<UpgradeID>,
+    } impl Context {
+        fn new() -> Self {
+            Self {
+                upgrade_id: None,
+            }
+        }
+    }
+
+    struct Response;
+    impl Response {
+        async fn send(self, _stream: &mut Stream) {
+            println!("[response] writing to stream");
+        }
+    }
+
+    struct Socket {
+        context: Context,
+        _stream:  Stream,
+    } impl Socket {
+        async fn handle_messages(&self) {
+            println!("[socket {}] handling messages",
+                self.context.upgrade_id.unwrap());
+        }
+        async fn close(self) {
+            println!("[socket {}] closing...",
+                self.context.upgrade_id.unwrap());
+        }
+    }
+
+    async fn handle(mut c: Context) -> (Response, Option<UpgradeID>) {
+        let requires_upgrade = true;
+        let mut upgrade_id   = None;
+
+        if requires_upgrade {
+            let id = reserve_upgrade().await;
+            upgrade_id   = Some(id);
+            c.upgrade_id = Some(id);
+        }
+
+        if requires_upgrade {
+            tokio::spawn(async move {
+                let Some(id) = c.upgrade_id
+                    else {panic!("Context doesn't have upgrade id")};
+
+                let socket = Socket {
+                    context: c,
+                    _stream:  assume_upgradable(id).await
+                };
+
+                socket.handle_messages().await;
+                socket.close().await;
+            });
+        }
+
+        (Response, upgrade_id)
+    }
+
+
+
+    for msecs in [42, 128, 64, 55, 60, 72, 111] {
+        let stream = Arc::new(Mutex::new({
+            tokio::time::sleep(tokio::time::Duration::from_millis(msecs)).await;
+            Stream
+        }));
+
+        match tokio::spawn({
+            let stream = stream.clone();
+            async move {
+                let stream = &mut *stream.lock().await;
+                let (res, upgrade_id) = handle(Context::new()).await;
+                res.send(stream).await;
+                upgrade_id
+            }
+        }).await {
+            Ok(upgrade_id) => {
+                if let Some(id) = upgrade_id {
+                    set_stream(id, stream).await
+                }
+            }
+            Err(e) => eprintln!("error: {e}")
+        }
+    }
 }
