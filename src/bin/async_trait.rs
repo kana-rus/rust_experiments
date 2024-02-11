b3c6dfcffed3012b837f929e6d0f513f90c6bd13
kana-rus <kanarus786@gmail.com>
2024-02-12 02:48:56 +0900

    @2024-02-12 02:48+9:00

--- src/bin/async_trait.rs
+++ src/bin/async_trait.rs
@@ -1,15 +1,31 @@
-use experiment::sample_async_trait;
+use experiment::sample_async_trait::{WithAsyncTrait, WithoutAsyncTrait};
+
 
 struct MyStruct;
 
 //#[async_trait::async_trait]
-impl sample_async_trait::WithAsyncTrait for MyStruct {
+impl WithAsyncTrait for MyStruct {
     fn f<'life0,'async_trait>(&'life0 self) ->  core::pin::Pin<Box<dyn core::future::Future<Output = String> + core::marker::Send+'async_trait> >where 'life0:'async_trait,Self:'async_trait {
-        todo!()
+        Box::pin(async {String::from("I'm with #[async_trait]!")})
     }        
 }
 
+impl WithoutAsyncTrait for MyStruct {
+    async fn f(&self) -> String {
+        String::from("I'm NOT with #[async_trait].")
+    }
+}
 
-fn main() {
 
+#[tokio::main]
+async fn main() {
+    tokio::task::spawn(async {
+        let s = <MyStruct as WithAsyncTrait>::f(&MyStruct).await;
+        println!("{s}");
+    }).await.unwrap();
+    
+    tokio::task::spawn(async {
+        let s = <MyStruct as WithoutAsyncTrait>::f(&MyStruct).await;
+        println!("{s}");
+    }).await.unwrap();
 }
--- src/sample_async_trait/mod.rs
+++ src/sample_async_trait/mod.rs
@@ -4,3 +4,7 @@ use async_trait::async_trait;
 pub trait WithAsyncTrait {
     async fn f(&self) -> String;
 }
+
+pub trait WithoutAsyncTrait {
+    async fn f(&self) -> String;
+}
